157ccf4813a45bba8fc643faa3689e8ab239ecee
Michaël Legrand <michael.c.legrand@gmail.com>
2018-04-16 09:09:12 +0200

    Generate documents component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { TaskComponent } from './task/task.component';
 import { LoginComponent } from './login/login.component';
 import { InformationsComponent } from './informations/informations.component';
+import { DocumentsComponent } from './documents/documents.component';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { InformationsComponent } from './informations/informations.component';
     HomepageComponent,
     TaskComponent,
     LoginComponent,
-    InformationsComponent
+    InformationsComponent,
+    DocumentsComponent
   ],
   imports: [
     BrowserModule,
