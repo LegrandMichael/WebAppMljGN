ea513068139d2f7d9c55a4b134d3fff2192b3ea3
Michaël Legrand <michael.c.legrand@gmail.com>
2018-04-17 10:46:44 +0200

    Authentication system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterializeModule } from 'angular2-materialize';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { TaskComponent } from './task/task.component';
-import { LoginComponent } from './login/login.component';
 import { InformationsComponent } from './informations/informations.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { TrainingComponent } from './training/training.component';
@@ -20,7 +20,6 @@ import { AuthenticationComponent } from './authentication/authentication.compone
     AppComponent,
     HomepageComponent,
     TaskComponent,
-    LoginComponent,
     InformationsComponent,
     DocumentsComponent,
     TrainingComponent,
@@ -30,7 +29,9 @@ import { AuthenticationComponent } from './authentication/authentication.compone
   ],
   imports: [
     BrowserModule,
-    MaterializeModule
+    MaterializeModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,3 +1,18 @@
 import { Routes, RouterModule } from '@angular/router';
 
-import { }
+import { HomepageComponent } from './homepage/homepage.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+
+const APP_ROUTES: Routes = [
+    {
+        path: '',
+        component: HomepageComponent
+    },
+    {
+       path: 'login',
+       component: AuthenticationComponent
+    },
+    { path: '**', redirectTo: '' }
+];
+
+export const Routing = RouterModule.forRoot(APP_ROUTES);
